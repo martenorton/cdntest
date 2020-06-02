225a059ce97603076ebc62229245f853a57bd5bf
martenorton <52880215+martenorton@users.noreply.github.com>
2020-06-02 18:13:43 +0800

    add mgr

---
+++ mgr/js/chunk-2d2221c4.b4da0f3b.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2221c4"],{cce3:function(n,e,c){"use strict";c.r(e);var t=function(){var n=this,e=n.$createElement,c=n._self._c||e;return c("div",[n._v("\n    推荐位视频\n")])},u=[],l=c("2877"),r={},s=Object(l["a"])(r,t,u,!1,null,null,null);e["default"]=s.exports}}]);
